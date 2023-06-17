568aa0d4722efd586a45eb086eb927469eca414a
dennisbabkin <dennis_babkin@hotmail.com>
2023-06-17 21:12:03 +0300

    Initial Commit

---
+++ macOS tips - part 2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  macOS tips - part 2
+//
+//  Created by Denn1s on 6/17/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
